af01eb6d07abbfa17efb7147beff7ee6946f2837
RobC <chung.robert@hotmail.com>
2024-01-17 16:07:26 -0600

    add validations for auth form

--- src/app/auth/auth.component.ts
+++ src/app/auth/auth.component.ts
@@ -1,4 +1,5 @@
 import { Component } from "@angular/core";
+import { FormBuilder, FormGroup, Validators } from "@angular/forms";
 
 @Component({
     selector: 'app-auth',
@@ -7,9 +8,21 @@ import { Component } from "@angular/core";
 export class AuthComponent {
 
     isLoginMode = true;
+    // form: FormGroup;
+
+    form = this.fb.group({
+        email: [null, [Validators.required, Validators.email]],
+        // Using Firebase's ReST API for auth requires at least 6 chars
+        password: [null, [Validators.required, Validators.minLength(6)]]
+    })
+    constructor(private fb: FormBuilder) { }
 
     switchMode() {
         this.isLoginMode = !this.isLoginMode
     }
 
+    onSubmit() {
+        console.log(this.form.value);
+        this.form.reset();
+    }
 }
