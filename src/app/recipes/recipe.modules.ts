bfd00cb39d120d6c0a090fd272d96630d2b4bc2f
RobC <chung.robert@hotmail.com>
2024-01-21 14:58:35 -0600

    added touch logic to shopping list qty input, organized code

--- src/app/recipes/recipe.modules.ts
+++ src/app/recipes/recipe.modules.ts
@@ -2,6 +2,7 @@ import { NgModule } from "@angular/core";
 import { RouterModule } from "@angular/router";
 import { CommonModule } from "@angular/common";
 import { ReactiveFormsModule } from "@angular/forms";
+import { RecipesRoutingModule } from "./recipes-routing.module";
 
 import { RecipesComponent } from "./recipes.component";
 import { RecipeDetailComponent } from "./recipe-detail/recipe-detail.component";
@@ -9,7 +10,6 @@ import { RecipeItemComponent } from "./recipe-list/recipe-item/recipe-item.compo
 import { RecipeListComponent } from "./recipe-list/recipe-list.component";
 import { RecipeEditComponent } from "./recipe-edit/recipe-edit.component";
 import { RecipeStartComponent } from "./recipe-start/recipe-start.component";
-import { RecipesRoutingModule } from "./recipes-routing.module";
 
 @NgModule({
     declarations:[    
--- src/app/shopping-list/shopping-list.module.ts
+++ src/app/shopping-list/shopping-list.module.ts
@@ -1,9 +1,10 @@
 import { NgModule } from "@angular/core";
-import { ShoppingEditComponent } from "./shopping-edit/shopping-edit.component";
-import { ShoppingListComponent } from "./shopping-list.component";
 import { ReactiveFormsModule } from "@angular/forms";
 import { CommonModule } from "@angular/common";
 import { ShoppingListRoutingModule } from "./shopping-list-routing.module";
+
+import { ShoppingEditComponent } from "./shopping-edit/shopping-edit.component";
+import { ShoppingListComponent } from "./shopping-list.component";
 
 @NgModule({
     declarations: [
