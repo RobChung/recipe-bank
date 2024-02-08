cb5f29638edeb7e2bcaad12e72713f91cf1273a0
RobC <chung.robert@hotmail.com>
2024-02-08 15:26:22 -0600

    load values into recipe edit form via store

--- src/app/recipes/recipe-edit/recipe-edit.component.ts
+++ src/app/recipes/recipe-edit/recipe-edit.component.ts
@@ -1,11 +1,10 @@
 import { Component, OnDestroy, OnInit } from '@angular/core';
 import { Form, FormArray, FormBuilder, FormControl, FormGroup, Validators } from '@angular/forms';
 import { ActivatedRoute, Params, Router } from '@angular/router';
-import { Subscription } from 'rxjs';
-import { Ingredient } from 'src/app/shared/ingredient.model';
+import { Subscription, map, switchMap } from 'rxjs';
 import { RecipeService } from '../service/recipe.service';
-import { Recipe } from '../recipe.model';
-import { DataStorageService } from 'src/app/shared/data-storage-service';
+import { Store } from '@ngrx/store';
+import * as fromApp from '../../store/app.reducer';
 
 @Component({
   selector: 'app-recipe-edit',
@@ -24,7 +23,7 @@ export class RecipeEditComponent implements OnInit, OnDestroy {
     private router: Router,
     private fb: FormBuilder,
     private recipeService: RecipeService,
-    private dataService: DataStorageService) { }
+    private store: Store<fromApp.AppState>) { }
 
   ngOnInit() {
     this.subscription = this.route.params.subscribe(
@@ -56,97 +55,51 @@ export class RecipeEditComponent implements OnInit, OnDestroy {
     // let recipeIngredients = [];
 
     if (this.editMode) {
-      const recipe = this.recipeService.getRecipe(this.id);
-      // console.log(recipe)
-      recipeName = recipe.name;
-      imgUrl = recipe.imagePath;
-      description = recipe.description;
-      // ingredients = recipe.ingredients;
-      // Theoretically, we can currently create a Recipe without Ingredients, so do a check
-        //recipe.ingredients?? recipe['ingredients']??
-      if (recipe.ingredients) {
-        // console.log('ingredients exist')
-        recipe.ingredients.forEach(ingredient => {
-          // console.log(ingredient)
-          
-          // Instead of pushing an ingredient, we need to push a FormGroup
-          // This is because we have two Controls that will control a single Ingredient
-          // recipeIngredients.push(ingredient);
+      // const recipe = this.recipeService.getRecipe(this.id);
+      this.store.select('recipes')
+        .pipe(
+          map(recipeState => {
+            // we want a single recipe for the given id
+            return recipeState.recipes[this.id]
+          })
+        )
+        .subscribe(recipe => {
+          recipeName = recipe.name;
+          imgUrl = recipe.imagePath;
+          description = recipe.description;
+          // Theoretically, we can currently create a Recipe without Ingredients, so do a check
+          if (recipe.ingredients) {
+            // console.log('ingredients exist')
+            recipe.ingredients.forEach(ingredient => {
 
-          // recipeIngredients.push(
-          //   new FormGroup({
-          //     'ingredientName': new FormControl(ingredient.name),
-          //     'ingredientQty': new FormControl(ingredient.quantity)
-          //   })
-          // )
-
-          // Check if using FB works
-          recipeIngredients.push(
-            this.fb.group({
-              name: [ingredient.name, Validators.required],
-              quantity: [ingredient.quantity, [Validators.required, Validators.min(1)]]
-            })
-          )
-
-        });
-      }
+              // Check if using FB works
+              recipeIngredients.push(
+                this.fb.group({
+                  name: [ingredient.name, Validators.required],
+                  quantity: [ingredient.quantity, [Validators.required, Validators.min(1)]]
+                })
+              )
+            });
+          }
+        })
     }
 
     this.recipeEditForm = this.fb.group({
       name: [recipeName, Validators.required],
       imagePath: [imgUrl, Validators.required],
       description: [description, Validators.required],
-      // Why is [] not required here??
-        // Because we're passing a FormArray??
       ingredients: recipeIngredients,
-
-      // Tried a bunch of bs here to validate input for name/qty, but the correct location was above where we create the form. This is because if recipe->ingredients exist, then those existing inputs need validation in them.
-      // In addition, we validate it in the 'add' function as new inputs rendered need the validation properties as well.
-      // ingredients: [recipeIngredients]
-      // ingredientName: ['', Validators.required],
-      // ingredientQty: [0, Validators.min(1)]
-      // ingredients:
-      //   this.fb.array(ingredientName, ingredientQty)
-      
-      // ingredients: this.fb.group({
-      //   data: this.fb.array([
-
-      //   ])
-      // })
-  
     })
   }
 
   onSubmit() {
 
-    // If the value of the form has the exact same format as the Recipe model, we can simply:
-    // this.recipeService.addRecipe(this.recipeEditForm.value)
-
-    // const recipeName = this.recipeEditForm.value.recipeName;
-    // const imgUrl = this.recipeEditForm.value.imgUrl;
-    // const description = this.recipeEditForm.value.recipeDescription;
-
-    // const formArrayData = this.recipeEditForm.value.ingredients;
-    
-    // const ingredientList: Ingredient[] = []
-    
-    // formArrayData.forEach(ingredient => {
-    //   let item = new Ingredient(ingredient.ingredientName, ingredient.ingredientQty)
-
-    //   ingredientList.push(item);
-    // });
-
-    // const recipe = new Recipe(recipeName, description, imgUrl, ingredientList);
-
     if (this.editMode) {
       this.recipeService.updateRecipe(this.id, this.recipeEditForm.value);
-      // See note in DataService Class
-      // this.dataService.updateRecipe(this.id, this.recipeEditForm.value);
     } else {
       this.recipeService.addRecipe(this.recipeEditForm.value);
     }
 
-    
     this.navigateBack();
 
   }
@@ -154,7 +107,7 @@ export class RecipeEditComponent implements OnInit, OnDestroy {
   onCancel() {
     this.navigateBack();
   }
-  
+
   additionalIngredient() {
     // we have more than 1 control to push
     // const control = new FormControl(null, Validators.required);
@@ -178,7 +131,7 @@ export class RecipeEditComponent implements OnInit, OnDestroy {
   }
 
   navigateBack() {
-    this.router.navigate(['../'], {relativeTo: this.route});
+    this.router.navigate(['../'], { relativeTo: this.route });
   }
 
   ngOnDestroy() {
